44bf530d4cf47f91b5d8e9cc4e4e0af4f63fb340
Vloods <lmvibe@outlook.com>
2025-05-10 20:29:03 +0300

    fix: db reset issue resolved

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:////app/db/test.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -11,5 +11,5 @@ Base = declarative_base()
 
 def recreate_tables():
     """Drop all tables and recreate them"""
-    Base.metadata.drop_all(bind=engine)
+#    Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
